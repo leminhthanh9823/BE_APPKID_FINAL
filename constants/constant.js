b4c5febc21c4b09bc28cba9882ef60e79b9600a4
leminhthanh9823 <thanhlm9823@fpt.edu.vn>
2025-09-27 00:09:24 +0700

    update constant

--- constants/constant.js
+++ constants/constant.js
@@ -1,11 +1,11 @@
 const GAME_TYPES = [
-  { value: 1, label: "wordle" },
-  { value: 2, label: "puzzle" },
-  { value: 3, label: "memory" },
-  { value: 4, label: "missing_word" },
-  { value: 5, label: "image_puzzle" },
-  { value: 6, label: "four_pics_one_word" },
-];
+  { value: 1, label: 'Wordle' },
+  // { value: 2, label: 'Puzzle' }, 
+  { value: 3, label: 'Memory Game' },
+  { value: 4, label: 'Missing Word' },
+  { value: 5, label: 'Image Puzzle' },
+  { value: 6, label: 'Four pictures one word' },
+];  
 
 module.exports = {
   GAME_TYPES,
